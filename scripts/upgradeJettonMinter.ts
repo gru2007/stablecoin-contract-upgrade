f3fe8dc0abfb6fb73a471a497dac7017ad26da14
Gru <ruslru376@gmail.com>
2025-02-05 11:55:03 +1000

    Update upgradeJettonMinter.ts

--- scripts/upgradeJettonMinter.ts
+++ scripts/upgradeJettonMinter.ts
@@ -12,8 +12,6 @@ export async function run(provider: NetworkProvider) {
     const jettonWalletCodeRaw = await compile('JettonWallet');
     const jettonWalletCode = jettonWalletCodeFromLibrary(jettonWalletCodeRaw);
 
-    const adminAddress = await promptUserFriendlyAddress("Enter the address of the jetton owner (admin):", ui, isTestnet);
-
     // e.g "https://bridge.ton.org/token/1/0x111111111117dC0aa78b770fA6A738034120C302.json"
     const jettonMetadataUri = await promptUrl("Enter jetton metadata uri (https://jettonowner.com/jetton.json)", ui)
     
